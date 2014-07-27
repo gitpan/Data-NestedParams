ac87652d67d77ca41d0ae3245a91b9c7590a806e
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2014-07-27 23:11:17 +0000

    Import of TOKUHIROM/Data-NestedParams-0.07 from CPAN.
    gitpan-cpan-distribution: Data-NestedParams
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         TOKUHIROM/Data-NestedParams-0.07.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Data/NestedParams.pm
+++ lib/Data/NestedParams.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use parent qw(Exporter);
 
@@ -17,7 +17,7 @@ sub parse {
 
     my @keys;
     while (1) {
-        if ($k =~ s/\[([a-zA-Z_-]+)\]\z//) {
+        if ($k =~ s/\[([a-zA-Z0-9_-]+)\]\z//) {
             unshift @keys, ['%', $1];
         } elsif ($k =~ s/\[\]\z//) {
             unshift @keys, ['@'];
